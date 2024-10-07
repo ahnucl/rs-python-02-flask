e30ae6e08ea8199199f83e4c84ddbe54dbe62c71
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2024-10-07 16:10:36 -0300

    listagen de tarefas

--- app.py
+++ app.py
@@ -16,5 +16,17 @@ def create_task():
     print(tasks)
     return jsonify({"message": "Nova tarefa criada com sucesso", "task": new_task.__dict__})
 
+
+@app.route('/tasks', methods=['GET'])
+def get_talks():
+    task_list = [task.to_dict() for task in tasks] 
+
+    output = {
+                "tasks": task_list,
+                "total_tasks": len(task_list)
+            }
+    return jsonify(output)
+
 if __name__ == '__main__': # Execução manual
     app.run(debug=True) # Apenas para desenvolvimento
+

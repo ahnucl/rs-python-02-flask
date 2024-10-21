90b81a1f7ba50750729b5b2bcffbe98773ece7be
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2024-10-21 15:02:55 +0000

    Cria testes de busca

--- tests.py
+++ tests.py
@@ -16,3 +16,24 @@ def test_create_task():
     assert "message" in response_body
     assert "id" in response_body
     tasks.append(response_body['id'])
+
+
+def test_get_tasks():
+    response = requests.get(f'{BASE_URL}/tasks')
+    assert response.status_code == 200
+    response_body = response.json()
+    assert 'tasks' in response_body
+    assert 'total_tasks' in response_body
+
+
+def test_get_task():
+    if tasks:
+        task_id = tasks[0]
+        response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+        assert response.status_code == 200
+        response_body = response.json()
+        assert task_id == response_body['id']
+
+
+def  test_update_task():
+    pass
